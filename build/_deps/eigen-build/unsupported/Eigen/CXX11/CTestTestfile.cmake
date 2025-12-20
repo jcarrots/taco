c8d81225295229d0c070e6d16619090a240c84d0
jcarrot <99557577+jcarrots@users.noreply.github.com>
2025-12-19 19:24:55 -0500

    Delete build directory

--- build/_deps/eigen-build/unsupported/Eigen/CXX11/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/59405/taco/build/_deps/eigen-src/unsupported/Eigen/CXX11
-# Build directory: C:/Users/59405/taco/build/_deps/eigen-build/unsupported/Eigen/CXX11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/_deps/eigen-build/unsupported/doc/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/59405/taco/build/_deps/eigen-src/unsupported/doc/examples
-# Build directory: C:/Users/59405/taco/build/_deps/eigen-build/unsupported/doc/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
